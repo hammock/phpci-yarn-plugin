5e16d30dcacc61cba2b1e778886c1f5e6406aad9
Aleksandr Ivanov <a.ivanov@software.travel>
2018-02-26 14:46:37 +0300

    Bump required php version

--- src/Yarn.php
+++ src/Yarn.php
@@ -12,7 +12,7 @@ class Yarn implements Plugin
     protected $directory;
     protected $phpci;
     protected $build;
-    protected $npm;
+    protected $yarn;
     protected $command;
     protected $flags;
 
@@ -27,7 +27,7 @@ class Yarn implements Plugin
         $this->build = $build;
         $this->phpci = $phpci;
         $this->directory = $path;
-        $this->npm = $this->phpci->findBinary('yarn');
+        $this->yarn = $this->phpci->findBinary('yarn');
         if (array_key_exists('directory', $options)) {
             $this->directory = $path . '/' . $options['directory'];
         }
@@ -60,7 +60,7 @@ class Yarn implements Plugin
         }
         $cmd .= ' %s && %s %s %s';
         // and execute it
-        return $this->phpci->executeCommand($cmd, $this->directory, $this->npm, $this->command, implode(' ', $flags));
+        return $this->phpci->executeCommand($cmd, $this->directory, $this->yarn, $this->command, implode(' ', $flags));
     }
 
 }
